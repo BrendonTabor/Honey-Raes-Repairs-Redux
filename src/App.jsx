8051152b5a018f7fe8ef87eefd65095dc452ed19
Brendon Tabor <Brendontabor@gmail.com>
2024-05-23 16:22:36 -0500

    Get All Tickets

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export const App = () => {
-  const [count, setCount] = useState(0)
+  const [allTickets, setAllTickets] = useState([])
+  const [emergencyOnly, setEmergencyOnly] = useState(false)
+  const [filteredTickets, setFilteredTickets] = useState([])
 
-  const handleButtonClick = () => {
-    setCount(count + 1)
-  }
+  useEffect(() => {
+    getAllTickets().then(setAllTickets)
+  }, [])
+
+  useEffect(() => {
+    emergencyOnly
+    ? setFilteredTickets(allTickets.filter(ticket => ticket.emergency))
+    : setFilteredTickets(allTickets)
+
+  }, [emergencyOnly])
+
+const getAllTickets = () => {
+  return fetch(`http://localhost:8088/serviceTickets`)
+  .then(res => {
+    return res.json()
+  })
+}
 
   return(
     <>
-      <button onClick={handleButtonClick}>
-        Increment
-      </button>
-      <div>Count: {count}</div>
+    <div className="container">
+      <button onClick={() => {setEmergencyOnly(!emergencyOnly)}}>Show Emergency Only!</button>
+      {filteredTickets.map(ticket => `${ticket.id}: ${ticket.description}`)}
+    </div>
     </>
   )
 }
